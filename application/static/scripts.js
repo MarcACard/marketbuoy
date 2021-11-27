15f3a1f4caa2c41d73cc2004a8452c71a2e03603
Marc Anthony Card <marcacard@gmail.com>
2021-11-26 22:36:30 -0700

    add boostrap & custom script to base

---
+++ application/static/scripts.js
@@ -0,0 +1,6 @@
+console.log("Hello World.")
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
